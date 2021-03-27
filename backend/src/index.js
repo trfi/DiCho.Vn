8ca2d91b5332e4f9906ddbe4fc24f07dcef196ef
tr.fi <gnourtf@gmail.com>
2021-03-27 20:12:26 +0700

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
+import jwt from 'jsonwebtoken';
+
+import resolvers from './resolvers';
+import { Prisma } from '../prisma/generated/prisma-client';
+
+import 'dotenv/config';
+
+const typeDefs = importSchema('src/schemas/user.graphql');
+
+export const db = new Prisma({
+  endpoint: process.env.PRISMA_ENDPOINT || 'http://localhost:4466',
+  secret: process.env.PRISMA_SECRET || '',
+});
+
+const getCurrentUser = async (request) => {
+    console.log(request.headers);
+    if (!request.headers.token) {
+        return null;
+    }
+    const user = await jwt.decode(
+        request.headers.token,
+        process.env.JWT_SECRET_KEY,
+    );
+    return { ...user };
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: async ({ request }) => {
+        const me = await getCurrentUser(request);
+        console.log(me);
+        return {
+          me,
+          prisma: db,
+        };
+    }
+});
+
+server.start({ port: process.env.PORT }, () => {
+    console.log('App running on http://localhost:4400');
+});
---
+++ backend/src/resolvers/index.js
@@ -0,0 +1,51 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { combineResolvers, skip } from 'graphql-resolvers';
+
+const userIsAuthenticated = (parent, args, { me }) => {
+    return me ? skip : new Error('Not authenticated');
+}
+
+export default {
+    Query: {
+        me: async (parent, { id }, { prisma, me }) => {
+            const user = await prisma.user({ id: me.user.id });
+            return user;
+        },
+        getUser: combineResolvers(
+            userIsAuthenticated,
+            async (parent, { id }, { prisma }) => {
+                const user = await prisma.user({ id });
+                return user;
+            }
+        ),
+        signIn: async (parent, { email, password }, { prisma }) => {
+            try {
+                const user = await prisma.user({ email });
+                if (!user) {
+                    throw new Error('Invalid credentials');
+                }
+                const passwordMatch = bcrypt.compareSync(password, user.password);
+                if (!passwordMatch) {
+                    throw new Error('Invalid credentials');
+                }
+                const token = jwt.sign({ user }, process.env.JWT_SECRET_KEY, { expiresIn: 36000 });
+                return { token };
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    Mutation: {
+        signUp: async (parent, { email, name, password }, { prisma }) => {
+            try {
+                const hashedPassword = bcrypt.hashSync(password, 12);
+                const user = await prisma.createUser({ email, name, password: hashedPassword });
+                const token = jwt.sign({ user }, process.env.JWT_SECRET_KEY, { expiresIn: 36000 });
+                return { token };
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    }
+}
